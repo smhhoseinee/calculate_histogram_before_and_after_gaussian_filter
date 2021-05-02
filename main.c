a170540be21f21d75968b92ef4502045b15e54de
Mohammad_Lakzaei <mohammad365@outlook.com>
2021-05-02 15:11:43 +0430

    Establishing a relationship between two main and c processes

--- main.c
+++ main.c
@@ -8,6 +8,13 @@ int main(){
 	//Variables
 	char *args[] = {"./histogram","./filtering", NULL};
 	char photo_address [100] ;
+	int fd1[2];
+	int fd2[2];
+	
+	
+	//Create two-way pips in order to exchange information between processe main and C 
+	pipe(fd1);
+	pipe(fd2);
 
 
 	//Create process A
@@ -22,6 +29,14 @@ int main(){
 	}
 	// Create process C
 	else if(fork() == 0){
+
+		char address [100] ;
+
+		//cominucate with main process to obtain photo address
+		close(fd1[1]);
+		read(fd1[0] , address , sizeof(address));
+		printf("Address is resived from main is: %s\n" , address);
+		
 		exit(0);
 		
 	}
@@ -33,6 +48,9 @@ int main(){
 	int stringlen = strlen(photo_address);
 	photo_address[stringlen - 1] = '\0'	;
 	
+	//Send photo address to process C
+	close(fd1[0]);
+	write(fd1[1] , photo_address ,sizeof(photo_address));
 	
 	printf("Address is: %s\n" , photo_address);
 	
